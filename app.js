e46278a29347c48ee083908c7ccb4a6ed6726908
Jony <tana_5678@hotmail.com>
2021-05-04 02:13:29 +0700

    test3

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ app.get('/',(req,res)=>{
     res.send('well donw! sadfsd');
 })
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port,()=>{
     console.log(`Sever open on port ${PORT}`);
 })
